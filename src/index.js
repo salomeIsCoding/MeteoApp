f8d95c40ad087a38fd3f50f9ec9f38fc3c567e47
salomeIsCoding <160015952+salomeIsCoding@users.noreply.github.com>
2024-02-22 13:19:19 +0700

    Added Axios

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ function search (event) {
     let searchInput = document.querySelector("#search-input-text");
     let cityElement = document.querySelector ("h1");
     cityElement.innerHTML = searchInput.value;
+
+    axios.get(apiUrl).then();
 }
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", search);
+
