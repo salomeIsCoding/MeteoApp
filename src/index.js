5c2e22eb9a2909c6827219e4b827bcbd04223f14
salomeIsCoding <160015952+salomeIsCoding@users.noreply.github.com>
2024-02-22 15:31:03 +0100

    Added function to display Temperature

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
+function displayWeather (response) {
+    let temperatureElement = document.querySelector(".weather-app-temperature");
+    let temperature = Math.round(response.data.temperature.current);
+    temperatureElement.innerHTML = temperature;
+    let cityElement = document.querySelector ("h1");
+    cityElement.innerHTML = response.data.city;
+}
+
 function searchCity(city){
     let apiKey = "te659a2ao0cb8e3d11cb64043bff9883";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     console.log(apiUrl);
-    axios.get(apiUrl).then();
+    axios.get(apiUrl).then(displayWeather);
 }
 function searchEngine (event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-input-text");
-    let cityElement = document.querySelector ("h1");
-    cityElement.innerHTML = searchInput.value;
     searchCity(searchInput.value);
 }
 
