304f9b19db30878e3082f3156514c80f06b0a721
salomeIsCoding <160015952+salomeIsCoding@users.noreply.github.com>
2024-02-22 13:14:26 +0700

    Renamed h1 variable in JS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 function search (event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-input-text");
-    let h1 = document.querySelector ("h1");
-    h1.innerHTML = searchInput.value;
+    let cityElement = document.querySelector ("h1");
+    cityElement.innerHTML = searchInput.value;
 }
 
 let form = document.querySelector("#search-form");
