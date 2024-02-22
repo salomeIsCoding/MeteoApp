6a6a2b2fad257fabda667cadd6317f688d0e04ca
salomeIsCoding <160015952+salomeIsCoding@users.noreply.github.com>
2024-02-22 13:31:00 +0700

    Called Api Url inside function "search"

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-function search (event) {
+function searchCity(city){
+    let apiKey = "te659a2ao0cb8e3d11cb64043bff9883";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then();
+}
+function searchEngine (event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-input-text");
     let cityElement = document.querySelector ("h1");
     cityElement.innerHTML = searchInput.value;
-
-    axios.get(apiUrl).then();
+    searchCity(searchInput.value);
 }
 
 let form = document.querySelector("#search-form");
-form.addEventListener("submit", search);
-
+form.addEventListener("submit", searchEngine);
