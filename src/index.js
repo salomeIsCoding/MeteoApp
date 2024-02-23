4649f2cd42085c2d9bc88f43d5df05c8bfbff071
salomeIsCoding <160015952+salomeIsCoding@users.noreply.github.com>
2024-02-23 04:54:17 +0100

    Added description Weather

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ function searchCity(city){
     console.log(apiUrl);
     axios.get(apiUrl).then(displayWeather);
 }
+
 function searchEngine (event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-input-text");
@@ -21,4 +22,41 @@ function searchEngine (event) {
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", searchEngine);
 
+// c'est la ville qui apparaît quand la page s'ouvre
 searchCity("Berlin");
+
+// function currentDate (date) {
+//     let date = new Date();
+//     let months = [
+//         "January",
+//         "February",
+//         "March",
+//         "April",
+//         "June",
+//         "July",
+//         "August",
+//         "September",
+//         "October",
+//         "November",
+//         "December",
+//     ];
+//     let month = months[date.getMonth()];
+//     let days = [
+//         "Sunday",
+//         "Monday",
+//         "Tuesday",
+//         "Wednesday",
+//         "Thursday",
+//         "Friday",
+//         "Saturday",
+//     ];
+//     let day = days[date.getDay()];
+//     let hours = date.getHours();
+//     if (hours < 10){
+//         hours = `0${hours}`;
+//      }  
+//      let minutes = date.getMinutes();
+//      if (minutes < 10){
+//         minutes = `0${minutes}`;
+//      }
+// }
